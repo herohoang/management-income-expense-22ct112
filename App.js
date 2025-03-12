1e692b969fd6da0b68f918a5e8aefd7157fa58da
Phiphi1718 <tangocphiphi1710@gmail.com>
2025-03-12 13:28:17 +0700

    Cập nhật tính năng login messages

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AuthContext from "./src/contexts/AuthContext";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import MainScreen from "./src/screens/MainScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import { TouchableOpacity, Image } from "react-native";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="AuthContext"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="AuthContext" component={AuthContext} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
